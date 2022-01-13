955880d92b8d095ba46946a8d74ba7a59b93bb48
emasdai <dikifarista61@gmail.com>
2022-01-13 07:14:58 +0700

    nambahin tentang kami tapi belom selesai

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,4 +4,20 @@
 
 @section('content')
     <h1 class="fw-bold text-center">Tentang Kami</h1>
+      <div class="container">
+        <h2 class="text-center text-uppercase text-white">Tentang BSOT</h2>
+        <div class="row">
+    		  <div class="col-md-2">
+    		  </div>
+          <div class="col-md-8 col-sm-auto">
+            <p class="text-center">
+            Barter-Bage adalah penghubung bagi sampah-sampah masyarakat dan bank sampah disekitarnya,
+            kami bekerja sama dengan lebih dari 5000 bank sampah diseluruh Indonesia, dengan penggalaman
+            lebih dari 3 tahun menjadi penghubung sekaligus mitra bank sampah
+            </p>
+          </div>
+    		  <div class="col-md-2">
+    		  </div>
+        </div>
+      </div>
 @endsection
