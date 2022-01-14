6163db2fc44e87a8e56dff75506f8bb36b3e1836
Alaric <alaric2001ra@gmail.com>
2022-01-14 21:26:23 +0700

    Titlebar Icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     @include('includes.meta')
     <title>@yield('title') | BarterBage</title>
 
+    <link rel="shortcut icon" type="image/png" href="https://drive.google.com/thumbnail?id=1OUnWMKBH_FCLq3D0keAbvMu_BFAQ_9y2">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
 </head>
