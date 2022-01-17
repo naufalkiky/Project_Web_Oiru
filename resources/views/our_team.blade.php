23cdd7a971dae4c0687dd5384f12a4c017d8e5af
maulanadrif <maulanadaffarifki@gmail.com>
2022-01-17 15:57:23 +0700

    our team

--- resources/views/our_team.blade.php
+++ resources/views/our_team.blade.php
@@ -5,6 +5,64 @@
 @section('content')
     <h1 class="fw-bold text-center mb-4">Our Team</h1>
     <div class="ms-0 ms-md-5 me-0 me-md-5">
-        
+        <div class="container" align="center">
+            <div class="row">
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://drive.google.com/thumbnail?id=15q52bB0mye_htUCBTuQmZ9_3tPiiE3d6" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Muhammad Ramadhan K.</b></h5>
+                      <p class="card-text">1202190178</p>
+                    </div>
+                </div>
+              </div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://drive.google.com/thumbnail?id=1_ExVV6t64azgyeEoJkE9fw2niH78tpYB" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Daffa Rifki Maulana</b></h5>
+                      <p class="card-text">1202190050</p>
+                    </div>
+                </div>
+              </div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://drive.google.com/thumbnail?id=1TZouNioCeocSy-NlbqbFsJbBj4GnTgUS" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Alaric Rasendriya Aniko</b></h5>
+                      <p class="card-text">1202194162</p>
+                    </div>
+                </div>
+              </div>
+              <div class="w-100"></div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://drive.google.com/thumbnail?id=1C07xEjVj1iD1mgCRQIN8-h4xP4-Q-ARJ" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Emas Dai Ashshiddiiqi</b></h5>
+                      <p class="card-text">1202190063</p>
+                    </div>
+                </div>
+              </div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Vina Fadillah</b></h5>
+                      <p class="card-text">1202194208</p>
+                    </div>
+                </div>
+              </div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Saroso Yuwono</b></h5>
+                      <p class="card-text">1202194190</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
 @endsection
