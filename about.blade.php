a93acb94cb0be7709241f0b3f5b19825c405e955
VinaFadillah <92071422+VinaFadillah@users.noreply.github.com>
2022-01-18 13:13:47 +0700

    new overview

---
+++ about.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+@section('title', 'Tentang Kami')
+
+@section('content')
+<h1 class="fw-bold text-center mb-4">Overview</h1>
+<div class="ms-0 ms-md-5 me-0 me-md-5">
+    <div class="col-sm-20 mb-2">
+        <div class="card shadow rounded-lg card-sembako">
+            <img src="/assets/img/groceries/tumpukan sampah.jpg" class="card-img-top" alt="groceries-image">
+            <p class="text-justify ms-0 ms-md-5 me-0 me-md-5">
+                <br>
+                Permasalahan sampah menjadi isu global yang mengkhawatirkan seiring perkembangan zaman di era modern.
+                Gaya hidup modernitas membuat penggunaan plastik semakin meningkat.
+                Namun, pemakaian produk plastik yang semakin bertambah ini tidak dibarengi dengan manajemen pengelolaan sampah plastik yang sesuai.
+                Akibatnya, kondisi ini memperburuk jumlah sampah plastik di dunia.
+                Negara-negara berikut merupakan penghasil sampah plastik terbesar di laut.
+                <br><br>
+                Indonesia Peringkat tiga sebagai negara yang menghasilkan sampah plastik terbesar di dunia diduduki oleh Indonesia.
+                Dijelaskan dalam laman indonesia.go.id, pada 2020, Indonesia menghasilkan sampah plastik sebanyak 67,8 juta ton atau terdapat 185.753 ton sampah setiap harinya dihasilkan oleh 270 juta penduduk.
+                Melansir laman forbes.com, Indonesia menghasilkan sampah plastik di laut sebanyak 56,3 juta kilogram, bahkan Sungai Citarum dinobatkan sebagai sungai paling tercemar di dunia.
+                <br>
+        </div>
+
+        <br>
+        <h1 class="fw-bold text-center mb-4">Our Solutions</h1>
+        <div class="col-sm-20 mb-2">
+            <div class="card shadow rounded-lg card-sembako">
+                <img src="/assets/img/groceries/antri sampah.jpg" class="card-img-top" alt="groceries-image">
+                <p class="text-justify ms-0 ms-md-5 me-0 me-md-5">
+                    <br>
+                    BarterBage adalah aplikasi yang dapat memudahkan masyarakat dalam
+                    menukarkan sampah daur ulang di lingkungannya (tempatnya) dengan
+                    sejumlah bage points untuk dikumpulkan atau ditukarkan langsung dengan
+                    paket sembako yang tersedia. Tujuan dibuatnya aplikasi website ini adalah
+                    untuk membantu mempermudah proses distribusi pengelolaan sampah non-organik
+                    yang dapat didaur ulang ke bank sampah, dengan imbalan bagi penyetor sampah yaitu
+                    sejumlah uang elektronik bernama bage points yang dapat ditukarkan dengan paket sembako
+                    yang tersedia sesuai dengan berat sampahnya.
+                    <br><br>
+                    Dengan aplikasi web ini diharapkan
+                    dapat membantu mengurangi penumpukan sampah, pencemaran lingkungan dan mendorong
+                    niat masyarakat untuk memilah sampah dengan tepat. Selain itu BarterBage adalah
+                    penghubung bagi sampah-sampah masyarakat dan bank sampah disekitarnya,kami bekerja sama
+                    dengan lebih dari 5000 bank sampah diseluruh Indonesia, dengan penggalaman lebih dari 3 tahun
+                    menjadi penghubung sekaligus mitra bank sampah.
+                </p>
+                <br>
+            </div>
+
+            <br>
+            <h1 class="fw-bold text-center mb-4">The Impact We Made</h1>
+            <div class="col-sm-20 mb-2">
+                <div class="card shadow rounded-lg card-sembako">
+                    <img src="/assets/img/groceries/desa bersih.jpg" class="card-img-top" alt="groceries-image">
+                    <p class="text-justify ms-0 ms-md-5 me-0 me-md-5">
+                        <br>
+                        Menjadi salah satu desa terbaik di Indonesia.
+                        Desa Penglipuran juga mendapat predikat sebagai desa terbersih ketiga di dunia, setelah Desa Mawlynnong di India dan Giethoorn di Belanda.
+                        <br><br>
+                        Berawal dari rasa peduli terhadap kebersihan lingkungannya, Masyarakat setempat hingga menyediakan tempat sampah setiap 30 meter di tiap ruas jalannya.
+                        Hal inilah yang kami inginkan untuk seluruh desa di Indonesia supaya dapat meningkatkan rasa peduli terhadap permasalahan sampah di kehidupan sehari-hari kita dan pentingnya menjaga kebersihan lingkungan sekitar.
+                    </p>
+                    <br>
+                </div>
+
+
+                @endsection
